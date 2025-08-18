19d388a68586f5dce079bd79047fa91215fa505d
tomfyfe <tomfyfe85@gmail.com>
2025-08-18 18:28:43 +0100

    basics

---
+++ beginners/index.js
@@ -0,0 +1,18 @@
+// primitive types:
+// String
+// Number
+// Boolean
+// undefined
+// null
+
+function greet(name, lastName) {
+  console.log("hi " + name + " " + lastName);
+}
+
+greet("tom", "fyfe");
+
+function square(num) {
+  return num * num;
+}
+
+console.log(square(2));
