06f291e89bfe507cf79fe22afacfeabb9841af93
tomfyfe <tomfyfe85@gmail.com>
2025-08-19 10:11:01 +0100

    assignment operators

--- beginners/index.js
+++ beginners/index.js
@@ -5,14 +5,4 @@
 // undefined
 // null
 
-function greet(name, lastName) {
-  console.log("hi " + name + " " + lastName);
-}
-
-greet("tom", "fyfe");
-
-function square(num) {
-  return num * num;
-}
-
-console.log(square(2));
+let x = 10;
