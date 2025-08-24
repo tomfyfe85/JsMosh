378b4accd5aab4ff81c25876741dcdabb871c2b3
tomfyfe <tomfyfe85@gmail.com>
2025-08-25 00:06:11 +0100

    for in loop

--- beginners/index.js
+++ beginners/index.js
@@ -2,8 +2,18 @@ for (let i = 0; i <= 5; i++) {
   if (i % 2 !== 0) console.log(i);
 }
 
-let i = 0;
-while (i <= 5) {
-  if (i % 2 !== 0) console.log(i);
-  i++;
-}
+// for
+// while
+// do-while
+
+// for-in loop
+const person = {
+  name: "Tom",
+  age: 30,
+};
+
+for (let key in person) console.log(key, person[key]);
+
+const colours = ["red", "green", "blue"];
+
+for (let i in colours) console.log(i, colours[i]);
