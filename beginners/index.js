45e4c76ddf8c7b51edfb7a5296e832411dd1c18e
tomfyfe <tomfyfe85@gmail.com>
2025-08-25 08:00:39 +0100

    ex 1

--- beginners/index.js
+++ beginners/index.js
@@ -1,19 +1,29 @@
-for (let i = 0; i <= 5; i++) {
-  if (i % 2 !== 0) console.log(i);
-}
+// for (let i = 0; i <= 5; i++) {
+//   if (i % 2 !== 0) console.log(i);
+// }
 
 // for
 // while
 // do-while
 
 // for-in loop
-const person = {
-  name: "Tom",
-  age: 30,
-};
+// const person = {
+//   name: "Tom",
+//   age: 30,
+// };
 
-for (let key in person) console.log(key, person[key]);
+// for (let key in person) console.log(key, person[key]);
 
-const colours = ["red", "green", "blue"];
+// const colours = ["red", "green", "blue"];
 
-for (let i in colours) console.log(i, colours[i]);
+// // for (let i in colours) console.log(i, colours[i]);
+
+// // for-of loop
+// for (let colour of colours) console.log(colour);
+
+let number = maximumFinder(1, 2);
+console.log(number);
+
+function maximumFinder(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
