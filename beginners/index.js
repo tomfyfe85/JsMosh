c4cbd8625da93f0fb157088b0bcbc3492b1bfe49
tomfyfe <tomfyfe85@gmail.com>
2025-08-26 08:13:42 +0100

    showNumbers

--- beginners/index.js
+++ beginners/index.js
@@ -1,16 +1,26 @@
-const result = checkSpeed(799);
+// const result = checkSpeed(799);
+// console.log(result);
+
+// function checkSpeed(speed) {
+//   const speedLimit = 70;
+//   const kmPerPoint = 5;
+
+//   if (speed <= speedLimit) {
+//     console.log("Ok");
+//     return;
+//   }
+
+//   let points = Math.floor((speed - speedLimit) / kmPerPoint);
+//   if (points >= 12) console.log("Licence suspended");
+//   else console.log(points + " points");
+// }
+
+const result = showNumbers(10);
 console.log(result);
 
-function checkSpeed(speed) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-
-  if (speed <= speedLimit) {
-    console.log("Ok");
-    return;
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? "EVEN" : "ODD";
+    console.log(i, message);
   }
-
-  let points = Math.floor((speed - speedLimit) / kmPerPoint);
-  if (points >= 12) console.log("Licence suspended");
-  else console.log(points + " points");
 }
